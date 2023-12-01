e83aa56e52c10c3c416f0995ffa526263b6866e1
saimraza321 <146342788+saimraza321@users.noreply.github.com>
2023-12-01 10:28:48 +0500

    Add files via upload

---
+++ scripy.js
@@ -0,0 +1,27 @@
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+// function next(){
+//   slides[index].classList.remove('active');
+//   index = (index + 1) % slides.length;
+//   slides[index].classList.add('active');
+// }
+
+// function prev(){
+//   slides[index].classList.remove('active');
+//   index = (index - 1 + slides.length) % slides.length;
+//   slides[index].classList.add('active');
+// }
